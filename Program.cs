e9c8658615858dc72a378491c9c983d44952ccb3
dxdns <dxdns@hotmail.com>
2024-08-16 16:27:30 -0300

    first

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("123", "x", "111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("321", "y", "222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
